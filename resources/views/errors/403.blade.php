d5b09b16c23ea6424d9e0569d6472e02fd1ebbda
Erick Zarate Hernandez <zaraterick@outlook.com>
2020-07-03 11:34:24 -0500

    Personalización de erores

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h3>403 - No tienes permisos para ver el recurso.</h3>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h3>404 - Página no encontrada</h3>
+@endsection
